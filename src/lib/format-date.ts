681bfd0f61daf980ae366aa1eb17d17103398213
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2024-08-26 08:06:10 +0530

    refactor: new nextjs application for new design

--- src/lib/format-date.ts
+++
@@ -1,8 +0,0 @@
-export const formatDate = (date: string) => {
-  return new Date(`${date}T00:00:00Z`).toLocaleDateString("en-US", {
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    timeZone: "UTC",
-  });
-};
